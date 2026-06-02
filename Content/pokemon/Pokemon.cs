2224c25abdbc236b1a758685b6c3162d4fdac863
Ninjaniloc <colin.baumanns@outlook.de>
2026-06-02 17:25:25 +0200

    Added Attacks & Types json

--- Content/pokemon/Pokemon.cs
+++ Content/pokemon/Pokemon.cs
@@ -5,10 +5,9 @@ public class Pokemon
     public Attack attack1;
     public Attack attack2;
     public Attack attack3;
-    public Attack attack4;
     public String name;
     public String gender;
     public int id;
-    public Type type = Type.Air;
+
 
 }
--- Content/pokemon/Type.cs
+++ Content/pokemon/Type.cs
@@ -1,18 +1,4 @@
 public enum Type
 {
-    Normal,
-    Fire,
-    Water,
-    Plant,
-    Electro,
-    Ice,
-    Fight,
-    Poison,
-    Ground,
-    Air,
-    Psycho,
-    Bug,
-    Rock,
-    Ghost,
-    Dragon
+
 }
