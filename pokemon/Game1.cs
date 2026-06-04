891e78fdff82d570f17b136ab0b5c686271deaf6
Mats Oeynhausen <m.oeynhausen@icloud.com>
2026-06-04 17:49:45 +0200

    playermodel test

--- pokemon/Game1.cs
+++ pokemon/Game1.cs
@@ -7,6 +7,8 @@ namespace pokemon;
 
 public class Game1 : Core
 {
+    private Texture2D _logo;
+
     public Game1()
         : base("Dungeon Slime", 320, 180, false) { }
 
@@ -19,7 +21,7 @@ public class Game1 : Core
 
     protected override void LoadContent()
     {
-        // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/player");
 
         base.LoadContent();
     }
@@ -41,7 +43,9 @@ public class Game1 : Core
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        SpriteBatch.Begin();
+        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+        SpriteBatch.End();
 
         base.Draw(gameTime);
     }
