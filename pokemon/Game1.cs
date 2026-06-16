891a930d6001f7c75e3177cb96dcd12f1c743ba8
Mats Oeynhausen <m.oeynhausen@icloud.com>
2026-06-16 08:26:49 +0200

    implemented input handling

--- pokemon/Game1.cs
+++ pokemon/Game1.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
 using MonoGameLibrary.Graphics;
+using MonoGameLibrary.Input;
 
 namespace pokemon;
 
 public class Game1 : Core
 {
     private Sprite _grass;
+    private Vector2 _grassPosition;
     private Sprite _stone;
+
+    private const float MOVEMENT_SPEED = 5.0f;
 
     public Game1()
         : base("Pokemon", 1920, 1080, true) { }
@@ -40,9 +45,39 @@ public class Game1 : Core
         )
             Exit();
 
-        // TODO: Add your update logic here
+        CheckKeyboardInput();
 
         base.Update(gameTime);
+    }
+
+    private void CheckKeyboardInput()
+    {
+        // If the space key is held down, the movement speed increases by 1.5
+        float speed = MOVEMENT_SPEED;
+
+        // If the W or Up keys are down, move the slime up on the screen.
+        if (Input.Keyboard.IsKeyDown(Keys.W) || Input.Keyboard.IsKeyDown(Keys.Up))
+        {
+            _grassPosition.Y -= speed;
+        }
+
+        // if the S or Down keys are down, move the slime down on the screen.
+        if (Input.Keyboard.IsKeyDown(Keys.S) || Input.Keyboard.IsKeyDown(Keys.Down))
+        {
+            _grassPosition.Y += speed;
+        }
+
+        // If the A or Left keys are down, move the slime left on the screen.
+        if (Input.Keyboard.IsKeyDown(Keys.A) || Input.Keyboard.IsKeyDown(Keys.Left))
+        {
+            _grassPosition.X -= speed;
+        }
+
+        // If the D or Right keys are down, move the slime right on the screen.
+        if (Input.Keyboard.IsKeyDown(Keys.D) || Input.Keyboard.IsKeyDown(Keys.Right))
+        {
+            _grassPosition.X += speed;
+        }
     }
 
     protected override void Draw(GameTime gameTime)
@@ -50,7 +85,7 @@ public class Game1 : Core
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         SpriteBatch.Begin();
-        _grass.Draw(SpriteBatch, Vector2.Zero);
+        _grass.Draw(SpriteBatch, _grassPosition);
         _stone.Draw(SpriteBatch, new Vector2(16, 0));
 
         SpriteBatch.End();
